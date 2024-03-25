71d5464b842ded681217cfaafede0218057b841d
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-03-24 17:50:44 -0700

    Quizgame

---
+++ quiz_game.py
@@ -0,0 +1,46 @@
+# Intro
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Lets Play")
+score = 0
+
+# Question 1
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 2
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 3
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Question 4
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+# Score
+print("You got " + str(score) + " questions correct!")
+print("You got " + str(score/4 * 100) + "%")
